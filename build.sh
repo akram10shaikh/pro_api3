867ff2eeefcff1b2fc9a6fd2fa2583d7871b6b9f
akram <akram10shaikh@gmail.com>
2024-11-26 21:10:07 +0530

    commit

--- build.sh
+++ build.sh
@@ -3,7 +3,17 @@
 set -o errexit
 
 pip install --upgrade pip
+pip install gunicorn
+pip install git+https://github.com/benoitc/gunicorn.git
+pip install -U git+https://github.com/benoitc/gunicorn.git
+pip install greenlet
+pip install eventlet
+pip install gunicorn[eventlet]
+pip install gevent
+pip install gunicorn[gevent]
+
 pip install -r requirements.txt
+
 
 
 python manage.py migrate
